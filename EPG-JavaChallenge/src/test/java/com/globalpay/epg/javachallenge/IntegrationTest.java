30fea36c4b0f8b81c7ddc32d9454f32e274f2733
cperezgo <christianperezglez@outlook.com>
2022-10-17 21:10:04 +0200

    UPDATE se limpia codigo

--- EPG-JavaChallenge/src/test/java/com/globalpay/epg/javachallenge/IntegrationTest.java
+++ EPG-JavaChallenge/src/test/java/com/globalpay/epg/javachallenge/IntegrationTest.java
@@ -9,10 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
